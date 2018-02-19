4138b6b88c27e5803748a97e9dee02be07c5a963
axisimski <33306491+axisimski@users.noreply.github.com>
2018-02-18 22:31:24 -0600

    //Basic Functionality (No 2's complement or error checking)

--- app/src/main/java/axisimski/binaryconverter/ToBinary.java
+++ app/src/main/java/axisimski/binaryconverter/ToBinary.java
@@ -5,4 +5,18 @@ package axisimski.binaryconverter;
  */
 
 public class ToBinary {
+
+    public void execute(){
+
+        if(!MainActivity.input.getText().toString().isEmpty()){
+
+            String value= MainActivity.input.getText().toString();
+            int n=Integer.parseInt(value);
+            String binaryString;
+            binaryString=Integer.toBinaryString(n);
+            MainActivity.output.setText(binaryString);
+            MainActivity.input.setText("");
+        } 
+    }
+
 }
