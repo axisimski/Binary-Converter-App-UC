10080e8f4cc8774c0fdcf2e64bf23bdb2638dd2d
axisimski <33306491+axisimski@users.noreply.github.com>
2018-02-22 16:40:50 -0600

    // Radio buttons mandated

--- app/src/main/java/axisimski/binaryconverter/ToBinary.java
+++ app/src/main/java/axisimski/binaryconverter/ToBinary.java
@@ -24,6 +24,37 @@ public class ToBinary {
             return str.toString();
         }
 
+        public String toTwosComplement(StringBuffer  str){
+
+            int n = str.length();
+
+            // Traverse the string to get first '1' from
+            // the last of string
+            int i;
+            for (i = n-1 ; i >= 0 ; i--)
+                if (str.charAt(i) == '1')
+                    break;
+
+            // If there exists no '1' concat 1 at the
+            // starting of string
+            if (i == 0)
+                return "1" + str;
+
+            // Continue traversal after the position of
+            // first '1'
+            for (int k = i-1 ; k >= 0; k--)
+            {
+                //Just flip the values
+                if (str.charAt(k) == '1')
+                    str.replace(k, k+1, "0");
+                else
+                    str.replace(k, k+1, "1");
+            }
+
+            // return the modified string
+            return str.toString();
+        }
+
 
 }
 
