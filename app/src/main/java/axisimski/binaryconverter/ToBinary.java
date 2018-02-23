e68745ebf6fd97e07462eb7f5cfa8bfa4f7878c1
axisimski <33306491+axisimski@users.noreply.github.com>
2018-02-22 22:46:45 -0600

    // Whole number 2's complement working (bar add 1)

--- app/src/main/java/axisimski/binaryconverter/ToBinary.java
+++ app/src/main/java/axisimski/binaryconverter/ToBinary.java
@@ -24,41 +24,72 @@ public class ToBinary {
             return str.toString();
         }
 
-
+//=================================================================Flip
         public String flip(String n){
 
             char[] myNameChars = n.toCharArray();
 
             for(int i=0;i<n.length();i++){
 
-                if(n.charAt(i)=='0'){
-                     myNameChars[i]='1';
+                if(n.charAt(i)=='.'){
+
+                    break;
                 }
+
+                if(n.charAt(i)=='0'){
+                    myNameChars[i]='1';
+                }
+
+                if(n.charAt(i)=='1'){
+                    myNameChars[i]='0';
+                }
+
             }
 
             n = String.valueOf(myNameChars);
 
 
+
             return n;
         }
 
+
+        //=============================================================================Convert to twos complement
         public String toTwosComplement(String  bin){
 
 
-        Character n=bin.charAt(0);
+            Character n=bin.charAt(0);
 
-        if(n!='-'){
+            if(n!='-'){
+              bin = "0"+bin;
+            }
 
-            bin = "0"+bin;
-        }
+            else if(n=='-'){
 
-         else if(n=='-'){
+              bin = bin.replace("-","");
 
-            bin = "00"+bin;
-          bin= flip(bin);
-            return bin;
+                bin = bin.substring(0, bin.indexOf('.'));
 
-        }
+
+                int digits=bin.length();
+
+
+              while(digits%4!=0){
+
+                  bin="0"+bin;
+                  digits=bin.length();
+
+
+              }
+
+
+
+
+             bin= flip(bin);
+
+              return bin;
+
+            }
 
 
             return  bin;
